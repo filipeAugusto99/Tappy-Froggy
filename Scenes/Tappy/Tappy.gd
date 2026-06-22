6851fe870dbf2976a4bab01fa531512f5e69934e
filipeAugusto99 <filipecordeiro213@gmail.com>
2026-06-22 12:16:48 -0300

    some comments and learning about Floor, Walls and ceilings

--- Scenes/Tappy/Tappy.gd
+++ Scenes/Tappy/Tappy.gd
@@ -3,12 +3,15 @@ extends CharacterBody2D
 
 const JUMP_FORCE = -350.0
 
+@onready var label: Label = $Label
 
 # Get the gravity.
 var _gravity: float = ProjectSettings.get('physics/2d/default_gravity')
+# Control when the play jump
 var _jumped: bool = false
 
 
+# Called when an input event was not handled by the UI or another node.
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed('jump'):
 		_jumped = true
@@ -22,14 +25,11 @@ func _physics_process(delta: float) -> void:
 	if _jumped:
 		velocity.y = JUMP_FORCE
 		_jumped = false
+	
+	# this method moves the CharacterBody while automatically handling collisions
+	# and sliding along walls and slopes.
 	move_and_slide()
-
-
-#func fly(delta: float) -> void:
-	## The body fall down with gravity
-	#velocity.y = delta * _gravity
-	#
-	#
-	#if Input.is_action_just_pressed('jump') == true:
-		#velocity.y = JUMP_VELOCITY
-	#
+	
+	label.text = "is_on_floor:%s\n" % is_on_floor()
+	label.text += "is_on_wall:%s\n" % is_on_wall()
+	label.text += "is_on_ceiling:%s" % is_on_ceiling()
