71a373cf6f5216a0a720382e824f09371ba6e27d
filipeAugusto99 <filipecordeiro213@gmail.com>
2026-06-24 12:43:00 -0300

    Pipes Scrolling

---
+++ Scenes/Pipe/Pipes.gd
@@ -0,0 +1,26 @@
+extends Node2D
+
+
+const SCROLL_VELOCITY: float = 120.0
+
+
+func _physics_process(delta: float) -> void:
+	# Scrolling the pipes
+	#move_local_x(delta * -SCROLL_VELOCITY) <- is possible too
+	position.x -= SCROLL_VELOCITY * delta
+
+
+# This function ensures that the object pipes leave the game for not consume more memory
+func die() -> void:
+	queue_free()
+	print("Pipe dies")
+
+
+# When the pipes exit the screen, emit this signal and the pipes "die"
+func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
+	die()
+
+	
+# Sometimes the signal _screen_exited doesn't work well, for ensure this, we create a timer
+func _on_timer_timeout() -> void:
+	queue_free()
